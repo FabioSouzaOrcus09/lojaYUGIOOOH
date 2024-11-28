c899051333cfb83bf47c0f99b560b394a65354f0
thiago-Felix-XD <thiago.souza398@etec.sp.gov.br>
2024-11-28 15:21:02 -0300

    segura esse w

--- yugioh/src/components/DecksGrid.vue
+++ yugioh/src/components/DecksGrid.vue
@@ -1,76 +1,76 @@
 <template>
-    <div Class="flex justify-center grid grid-cols-4 gap-0 ml-15">
+    <div Class="flex gap-1 grid grid-cols-4">
 
         <div id="grid1">
             <img id="Albaz" src="@/assets/Decks de estrutura/Albaz.png" 
             alt="Albaz" width="135" height="180"
-            class="flex object-contain " />
+            class="flex object-contain hover " />
         </div>
 
         <div id="grid2">
             <img id="Sombrio" src="@/assets/Decks de estrutura/ConfrontoSombrio.png" 
             alt="Sombrio" width="135" height="180" 
-            class="flex object-contain " />
+            class="flex object-contain hover " />
         </div>
 
         <div id="grid3">
             <img id="Glaciais" src="@/assets/Decks de estrutura/CorrentesGlaciais.png" 
             alt="Glaciais" width="135" height="180" 
-            class="flex object-contain " />
+            class="flex object-contain hover " />
         </div>
 
         <div id="grid4">
             <img id="CyberStrike" src="@/assets/Decks de estrutura/CyberStrike.png" 
             alt="CyberStrike" width="135" height="180" 
-            class="flex object-contain " />
+            class="flex object-contain hover " />
         </div>
 
         <div id="grid5">
             <img id="Engrenagens" src="@/assets/Decks de estrutura/EngrenagensDoPoder.png" 
             alt="Engrenagens" width="90" height="140" 
-            class="flex object-contain, justify-center, ml-7" />
+            class="flex object-contain, , ml-7 hover" />
         </div>
 
         <div id="grid6">
             <img id="FaíscasdeJustiça" src="@/assets/Decks de estrutura/FaíscasdeJustiça.png"
             alt="FaíscasdeJustiça" width="90" height="140" 
-            class="flex object-contain, justify-center, ml-7" />
+            class="flex object-contain, , ml-7 hover" />
         </div>
 
         <div id="grid7">
             <img id="FireKings" src="@/assets/Decks de estrutura/FireKings.png" 
-            alt="FireKings" width="135" height="180"
-            class="flex object-contain justify-center" />
+            alt="FireKings" width="120" height="165"
+            class="flex object-contain  ml-4 hover" />
         </div>
 
         <div id="grid8">
             <img id="FúriadoDinosmagador" src="@/assets/Decks de estrutura/FúriadoDinosmagador.png"
             alt="FúriadoDinosmagador" width="135" height="180" 
-            class="flex object-contain justify-center" />
+            class="flex object-contain  hover" />
         </div>
 
         <div id="grid9">
             <img id="ImperadordasTrevas" src="@/assets/Decks de estrutura/ImperadordasTrevas.png"
             alt="ImperadordasTrevas" width="135" height="180" 
-            class="flex object-contain justify-center" />
+            class="flex object-contain  hover" />
         </div>
 
         <div id="grid10">
             <img id="Investidadosreisdefogo" src="@/assets/Decks de estrutura/Investidadosreisdefogo.png" 
             alt="Investidadosreisdefogo" width="135" height="180" 
-            class="flex object-contain justify-center" />
+            class="flex object-contain  hover" />
         </div>
 
         <div id="grid11">
             <img id="LinkCiberso" src="@/assets/Decks de estrutura/LinkCiberso.png" 
             alt="LinkCiberso" width="135" height="180" 
-            class="flex object-contain justify-center" />
+            class="flex object-contain  ml-3 hover" />
         </div>
 
         <div id="grid12">
-            <img id="ZombieHorde" src="@/assets/Decks de estrutura/ZombieHorde.png"
-            alt="ZombieHorde" wwidth="20" height="50" 
-            class="flex object-contain justify-center" />
+            <img  id="ZombieHorde" src="@/assets/Decks de estrutura/ZombieHorde.png" 
+            alt="ZombieHorde" width="135" height="180" 
+            class="flex object-contain  hover" />
         </div>
     </div>
 
